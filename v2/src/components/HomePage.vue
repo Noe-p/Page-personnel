a369a891310451345f724bf051f95b88a428e65e
Noe-p <noephilippe29@gmail.com>
2021-07-31 10:35:16 +0200

    Ajout du component Project

--- v2/src/components/HomePage.vue
+++ v2/src/components/HomePage.vue
@@ -1,69 +1,47 @@
 <template>
 
-  <Navbar/>
+<Navbar/>
 
-  <Header/>
+<Header/>
 
 <article id="projects">
   <h1>Some projects...</h1>
-  <div>
-    <h2>Raconte</h2>
-    <a target='blank' href="https://raconte-reportage.com/index.html">
-      <img src="../assets/img/raconte.png" alt='Raconte'>
-      <section class="languages">
-        <img src="../assets/logo/html.png" alt="html">
-        <img src="../assets/logo/css.png" alt="css">
-        <img src="../assets/logo/javaScript.png" alt="javaScript">
-      </section>
-    </a>
-  </div>
-  <div>
-    <h2>Todo List</h2>
-    <a target='blank' href="https://github.com/Noe-p/todo-list">
-      <img src="../assets/img/todoApp.png" alt='todoApp'>
-      <section class="languages">
-        <img src="../assets/logo/html.png" alt="html">
-        <img src="../assets/logo/css.png" alt="css">
-        <img src="../assets/logo/javaScript.png" alt="javaScript">
-        <img class="framework" src="../assets/logo/vue.png" alt="vue">
-      </section>
-    </a>
-  </div>
-  <div>
-    <h2>Batz</h2>
-    <a target='blank' href="https://location-maison-iledebatz.fr/">
-      <img src="../assets/img/batz.png" alt='Batz'>
-      <section class="languages">
-        <img src="../assets/logo/html.png" alt="html">
-        <img src="../assets/logo/css.png" alt="css">
-        <img src="../assets/logo/javaScript.png" alt="javaScript">
-      </section>
-    </a>
-  </div>
-  <div>
-    <h2>Focus</h2>
-    <a target='blank' href="https://github.com/Noe-p/Developpement-application-web">
-      <img src="../assets/img/focus.png" alt='Focus'>
-      <section class="languages">
-        <img src="../assets/logo/html.png" alt="html">
-        <img src="../assets/logo/css.png" alt="css">
-        <img src="../assets/logo/javaScript.png" alt="javaScript">
-        <img src="../assets/logo/php.png" alt="php">
-        <img src="../assets/logo/sql.png" alt="sql">
-      </section>
-    </a>
-  </div>
-  <div>
-    <h2>Space Invaders</h2>
-    <a target='blank' href="https://github.com/Noe-p/Space-Invaders">
-      <img src="../assets/img/spaceInvaders.png" alt='spaceInvaders'>
-      <section class="languages">
-        <img src="../assets/logo/python.png" alt="python">
-      </section>
-    </a>
-  </div>
-</article>
 
+  <Project
+    title="Raconte"
+    img="raconte.png"
+    v-bind:langs="['html', 'css', 'javaScript']"
+    v-bind:framework="[]"
+  />
+
+  <Project
+    title="Todo List"
+    img="todoApp.png"
+    v-bind:langs="['html', 'css', 'javaScript']"
+    v-bind:framework="['vue']"
+  />
+
+  <Project
+    title="Batz"
+    img="batz.png"
+    v-bind:langs="['html', 'css', 'javaScript']"
+    v-bind:framework="[]"
+  />
+
+  <Project
+    title="Focus"
+    img="focus.png"
+    v-bind:langs="['html', 'css', 'javaScript', 'php', 'sql']"
+    v-bind:framework="[]"
+  />
+
+  <Project
+    title="Space Invaders"
+    img="spaceInvaders.png"
+    v-bind:langs="['python']"
+    v-bind:framework="[]"
+  />
+</article>
 
 <About/>
 
@@ -77,16 +55,32 @@
 import Navbar from './Navbar.vue';
 import Header from './Header.vue';
 import About from './About.vue';
-
+import Project from './Project.vue';
 
 export default {
   name: 'HomePage',
   components: {
     Navbar,
     Header,
-    About
+    About,
+    Project
   }
 }
 </script>
 
-<style scoped lang="scss" src="../style/main.scss"></style>
+<style scoped lang="scss" src="../style/main.scss">
+#projects {
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: rgb(28, 28, 28);
+  margin-top: 50px;
+}
+
+#projects h1 {
+  margin: 0;
+}
+
+
+</style>
---
+++ v2/src/components/Project.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="project-item">
+    <h2>{{ title }}</h2>
+    <a target='blank' href="https://raconte-reportage.com/index.html">
+      <img :src="require(`@/assets/img/${img}`)" :alt='[[ title ]]'>
+      <section  class="languages">
+        <div v-for="lang in langs" v-bind:key="lang">
+          <img :src="require(`@/assets/logo/${lang}.png`)" :alt='[[ lang ]]'>
+        </div>
+        <div v-for="item in framework" v-bind:key="item">
+          <img class="framework" :src="require(`@/assets/logo/${item}.png`)" :alt='[[ item ]]'>
+        </div>
+    </section>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'Project',
+    props: {
+      title: String,
+      img: String,
+      langs: Array,
+      framework: Array
+    }
+
+  }
+</script>
+
+<style media="screen">
+
+.project-item {
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#projects h2 {
+  font-size: 35px;
+}
+
+#projects a img {
+  width: 80vw;
+  transition: all 0.2s;
+  border-radius: 5px;
+  box-shadow: 1px 1px 10px rgb(18, 18, 18);
+}
+
+#projects a img:hover {
+  box-shadow: 2px 2px 20px rgb(18, 18, 18);
+}
+
+#projects a section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 110px;
+  left: 50%;
+  transform: translate(-50%,-130%);
+  transition: all 0.5s;
+  z-index: -5;
+  position: absolute;
+}
+
+#projects a section img {
+  border-radius: none;
+  box-shadow: none;
+  width: 35px;
+  margin: 5px 10px 0;
+}
+
+#projects a section .framework {
+  margin-left: 60px;
+}
+
+#projects a:hover section {
+  transform: translate(-50%,0);
+}
+
+</style>
